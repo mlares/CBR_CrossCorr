b44d07d70b70fd12e75b31566b63191fe3a38806
Marcelo Lares <marcelo.lares@gmail.com>
2019-11-08 16:13:30 -0300

    developing the PixelSky module

--- src/PixelSky.py
+++ src/PixelSky.py
@@ -53,12 +53,28 @@ class SkyMap():
 
     import healpy as hp
 
-    def __init__(self, nside):
+    def __init__(self, nside=256, ordering='ring', frame='equatorial'):
+
+        import healpy as hp
+
         self.nside = nside
+        self.ordering = 'ring'
+        self.frame = 'equatorial'
+
+        self.npixs = hp.nside2npix(self.nside)
 
         # fac =
         # Dsel: sample of galaxies
         # self, skymap, nside,fac,rprof,Dsel,vec,hp_data_sel,hp_mask):
+
+    def __len__(self):
+        return self.npixs
+
+    def __repr__(self):
+        return 'Sky Map with {!s} pixels in {!s} order'.format(self.npixs, self.ordering)
+
+    def __str__(self):
+        return 'Sky Map with {!s} pixels in {!s} order'.format(self.npixs, self.ordering)
 
     def load(self, filename, *args, **kwargs):
         '''
@@ -78,6 +94,18 @@ class SkyMap():
 
         d = hp.read_map(filename, h=True, **kwargs)
         return(d)
+    
+    def apply_mask(self, mask):
+
+        import healpy as hp
+
+        m = self[0].copy()
+        k = mask[0].copy()
+        m[k<0.5] = hp.UNSEEN
+        masked_map = hp.ma(m)
+        return(masked_map)
+
+
     # }}}
 
 
@@ -111,54 +139,34 @@ class RadialProfile():
         self.breaks = np.linspace(*args, **kwargs)
         self.breaks = self.breaks * unit
 
-    def radialprofile(self, skymap, centers_catalog):
-
+    def radialprofile(self, skymap, skymask, centers_catalog):
         """radialprofile(self, skymap) : computes the stacked radial profile of
         CMB pixels around selected centers
+
         Tasks:
-        1. traverse all radial bins
-        2. traverse all centers
+        1. traverse all centers (paralalize here)
+        2. traverse all radial bins
         3. traverse all pixels in the ring
         4. compute the mean
         5. store the mean values for all the rings
 
         Args:
-
-        skymap (class SkyMap):
+            skymap (class SkyMap):
             Map of the cosmic background, including scalar and mask
 
-        centers_catalog (class Centers):
+            centers_catalog (class Centers):
             Catalog of the centers, including (x, y, z) position
             in Healpix convention and position angle of the galaxy
             disk.
 
-        VIEJO:
-
-        nside,fac,rprof,Dsel,vec,hp_data_sel,hp_mask):
-        nside (int): the healpix nside parameter
-        rprof = breaks
-        vec = lista de centros
-        fac =
-        Dsel: sample of galaxies
-        hp_data_sel: temperature
-        hp_mask; mask
-
-        nside  --> skymap.nside
-        rprof  --> self.breaks
-        vec    --> centers_catalog.pos
-        fac
-        Dsel   --> centers_catalog.....
-        hp_data_sel --> CMB temperature map
-        hp_mask     --> CMB mask
-
         Raises:
-        errors?
+            errors?
 
         Returns:
-        profdata:
-        proferror:
-        uncertaintydata:
-        uncertaintyerror:
+            profdata:
+            proferror:
+            uncertaintydata:
+            uncertaintyerror:
         """
         import numpy as np
         import healpy as hp
@@ -174,6 +182,7 @@ class RadialProfile():
         for center in range(glxcat.vec):
 
             listpixs_internal = []
+            listpixs_mask = []
 
             for radiusfloat in radiifloat:
 
@@ -187,6 +196,7 @@ class RadialProfile():
 
                 listpixs_ring = list(set(listpixs_external) -
                                      set(listpixs_internal))
+                listpixs_mask = skymask[0][listpixs_ring]
                 mean_ring = np.nanmean(skymap[listpix_ring])
                 profdata.append(mean_ring)
                 listpixs_internal = listpixs_external.copy()
--- src/run.py
+++ src/run.py
@@ -3,21 +3,31 @@ import PixelSky as pxs
 import pandas as pd
 
 # Read CMB temperature map
+mapa = pxs.SkyMap(nside)
+mask = pxs.SkyMap(nside)
+
 filename = '../dat/lensmap512_10arcmin_y2.fits'
-nside = 256
-mp = pxs.SkyMap(nside)
 mapa = mp.load(filename, field=(0))
+filename = '../dat/lensmask512_10arcmin_y2.fits'
+mask = mp.load(filename, field=(0))
+
+# mapa.apply_mask(mask) # no funciona??
+
+
 
 # Read galaxy catalog
 glx_catalog = '../dat/2mrs_1175_done.dat'
 glx = pd.read_csv(glx_catalog, delim_whitespace=True, header=9)
-http://tdc-www.harvard.edu/2mrs/2mrs_readme.html
+# catalog: http://tdc-www.harvard.edu/2mrs/2mrs_readme.html
 
 # filter galaxy catalog
 l = ['A','X','B']
 spiral = [any([s in x for s in l]) for x in glx['type']]
 edgeon = glx['b/a'] < 0.8
 subset = spiral & edgeon
+
+rp = pxs.RadialProfile()
+rp.set_breaks(unit=1., start=0., stop=30., num=10)
 
 
 
